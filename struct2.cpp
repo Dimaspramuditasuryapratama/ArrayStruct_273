72eb693a5914b56b707ae5710e7149976cc752e3
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-04-15 11:43:45 +0700

    membuat detailAlamat mahasiswa

--- struct2.cpp
+++ struct2.cpp
@@ -7,3 +7,30 @@ struct detailAlamat
     string kota;
 };
 
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan nim = ";
+    cin >> mhs.nim;
+    cout << "Masukkan nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan alamat = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat kota = ";
+    cin >> mhs.alamat.kota;
+
+    // menampilkan data struct
+    cout << endl;
+    cout << "nim = " << mhs.nim << endl;
+    cout << "nama = " << mhs.nama << endl;
+    cout << "alamat = " << mhs.alamat.desa<< endl;
+    cout << "kota = " << mhs.alamat.kota << endl;
+    
+}
