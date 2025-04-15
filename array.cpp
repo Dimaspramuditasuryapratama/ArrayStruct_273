fe340ca038a7a6b6d747be6ef584fb31f77eec37
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-04-15 11:03:23 +0700

    membuat deklarasi dengan ukuran dan diberi nilai awal

--- array.cpp
+++ array.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
 
-//deklarasi array ukuran, wajib langsung diisi data
+//deklarasi array tanpa ukuran, wajib langsung diisi data
 int nilai[]={1,2,3};
+
+//deklarasi array dengan dengan ukuran, dan diberi nilai di awal
+int skor[4]={9,13,20,11};
