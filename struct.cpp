e08e4753487fe821c9644e886067a81697eeabe2
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-04-15 11:30:58 +0700

    membuat menampilkan data struct

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,25 @@ struct mahasiswa
     string nama;
     string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "202201401102";
+    mhs.nama = "Abra Yudhistira";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan nim = ";
+    cin >> mhs.nim;
+    cout << "Masukkan nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan alamat = ";
+    cin >> mhs.alamat;
+
+    // menampilkan data struct
+    cout << endl;
+    cout << "nim = " << mhs.nim << endl;
+    cout << "nama = " << mhs.nama << endl;
+    cout << "alamat = " << mhs.alamat << endl;
+    
+}
